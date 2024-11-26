2b9c05de4260d621e8dcd08a4c0becbac9ceee0a
DonShanilka <shanilka799.com>
2024-11-26 08:51:28 -0800

    create loadAllLog

--- js/Log/LogPage.js
+++ js/Log/LogPage.js
@@ -1,3 +1,56 @@
+initializeCrop()
+
+function initializeCrop(){
+    loadLogTable()
+}
+
+function loadLogTable(){
+    $.ajax({
+        url: "http://localhost:5050/greenshow/api/v1/log",
+        type: "GET",
+        success: (res) => {
+            addLogToTable(res.data);
+            console.log(res.data);
+        },
+        error: (res) => {
+            console.error(res);
+        }
+    });
+    
+}
+
+function addLogToTable(log) {
+    // Get the table body element
+    const logTableBody = document.getElementById("logTableBody");
+
+    // Clear any existing rows in the table
+    logTableBody.innerHTML = "";
+
+    // Loop through each crop and create a table row
+    log.forEach(log => {
+        // Create a new table row element
+        const row = document.createElement("tr");
+
+        // Construct the row HTML
+        row.innerHTML = `
+            <td class="px-6 py-4">${log.logCode || 'N/A'}</td>
+            <td class="px-6 py-4">${log.logDate || 'N/A'}</td>
+            <td class="px-6 py-4">${log.logDetails || 'N/A'}</td>
+            <td class="px-6 py-4"><img src="${log.image || 'crop image'}"class="w-12 h-12 rounded-md object-cover"></td>
+            <td class="px-6 py-4">
+                <button class="bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-700" onclick="editCrop('${log.id}')">
+                <i class="fas fa-edit text-lg"></i>
+                </button>
+                <button class="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-700" onclick="deleteCrop('${log.id}')">
+                <i class="fas fa-trash text-lg"></i>
+                </button>
+            </td>
+        `;
+
+        // Append the row to the table body
+        logTableBody.appendChild(row);
+    });
+}
 
 
 //  Save Log
@@ -34,3 +87,5 @@ document.getElementById('logForm').addEventListener('submit', function(e) {
         }
     });
 });
+
+
